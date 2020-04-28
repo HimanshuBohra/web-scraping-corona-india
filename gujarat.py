7b1a285ffeffbb1988b680d7db820bbd3fa8435b
HimanshuBohra <39338459+HimanshuBohra@users.noreply.github.com>
2020-04-28 13:18:26 +0530

    Update gujarat.py

--- gujarat.py
+++ gujarat.py
@@ -4,8 +4,6 @@ from bs4 import BeautifulSoup as soup
 import matplotlib.pyplot as plt; plt.rcdefaults()
 import numpy as np
 import matplotlib.pyplot as plt
-
-plt.style.use('ggplot')
 url = 'https://gujcovid19.gujarat.gov.in/'
 #openning connection,grabbing page
 uClient = uReq(url)
@@ -16,18 +14,50 @@ uClient.close()
 page_soup = soup(page_html,"html.parser")
 # has each district names is gujarat    
 district_name = page_soup.findAll("td",{"class":"text-left"})
-district_row = page_soup.findAll("span",{"class":"text-blue font-weight-bold"})
-#print(district_row[2].span)
+district_quarantine = page_soup.findAll("span",{"class":"text-blue font-weight-bold"})
+district_confirmed = page_soup.findAll("span",{"class":"text-red font-weight-bold"})
+district_tested = page_soup.findAll("span",{"class":"text-orange font-weight-bold"})
+district_recovered = page_soup.findAll("span",{"class":"text-success font-weight-bold"})
+district_death = page_soup.findAll("span",{"class":"font-weight-bold"})
+# bar graph for district wise quarantined patients in gujarat
 x = []
-y = []
+y_quaran = []
 for i in district_name:
         x.append(i.text)
-for i in district_row:
-        y.append(int(i.text))
-#print(max(y))
-#index = np.arange(0,max(y),500)
-#print(index)
+
+#bar graph for district wise quarantined patients
+for i in district_quarantine:
+        y_quaran.append(int(i.text))
 index = np.arange(len(x))
-plt.bar(index,y)
+plt.bar(index,y_quaran)
+plt.xticks(index,x,fontsize=5,rotation=45)
+plt.show()
+#bar graph for district wise confirmed cases in gujarat
+y_confirm = []
+for i in district_confirmed:
+        y_confirm.append(int(i.text))
+plt.bar(index,y_confirm)
+plt.xticks(index,x,fontsize=5,rotation=45)
+plt.show()
+
+#bar graph for district wise tested patients
+y_tested = []
+for i in district_tested:
+        y_tested.append(int(i.text))
+plt.bar(index,y_tested)
+plt.xticks(index,x,fontsize=5,rotation=45)
+plt.show()
+#bar graph for district wise recovered patients
+y_recovered = []
+for i in district_recovered:
+        y_recovered.append(int(i.text))
+plt.bar(index,y_recovered)
+plt.xticks(index,x,fontsize=5,rotation=45)
+plt.show()
+#bar graph for district wise deaths 
+y_death = []
+for i in district_death:
+        y_death.append(int(i.text))
+plt.bar(index,y_death)
 plt.xticks(index,x,fontsize=5,rotation=45)
 plt.show()
